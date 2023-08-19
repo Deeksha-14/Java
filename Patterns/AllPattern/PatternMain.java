ad954599822288c038f1dd4b71e000e671bc0583
DEEKSHA KUSHWAH <deekshakushwah0@gmail.com>
2023-08-20 01:09:50 +0530

    Update PatternMain.java

--- Patterns/AllPattern/PatternMain.java
+++ Patterns/AllPattern/PatternMain.java
@@ -7,6 +7,12 @@ public class PatternMain {
 	
 		patternMaker.numberTriangular();
 
+		System.out.println("ENter number of n for hollow triangle : ");
+		int n = sc.nextInt();
+		patternMaker.hollowSquare(n);
+		
+		sc.close();
+
 	}
 
 }
